c78e3f7ac5c3d3252fe6a0b0e0bcdbd63bedbcd7
Christoph Schmid <christoph.schmid2@cgm.com>
2024-10-04 13:42:55 +0200

    Test

--- src/app/preventive-medical-checkup/preventive-medical-checkup.component.ts
+++ src/app/preventive-medical-checkup/preventive-medical-checkup.component.ts
@@ -1,12 +1,13 @@
-import {Component, HostListener, OnInit, ViewEncapsulation} from '@angular/core';
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {OverviewComponent} from "../overview/overview.component";
-import {PatientHeaderComponent} from "../patient-header/patient-header.component";
-import {VitalSignsComponent} from "../vital-signs/vital-signs.component";
-import {GlobalSearchFieldComponent} from "../patient-header/global-search-field/global-search-field.component";
-import {NgIf} from "@angular/common";
-import {HelpService} from "../services/help.service";
-import {HelpComponent} from "../shared/help/help.component";
+
+import { Component, HostListener, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatTab, MatTabGroup } from "@angular/material/tabs";
+import { OverviewComponent } from "../overview/overview.component";
+import { PatientHeaderComponent } from "../patient-header/patient-header.component";
+import { VitalSignsComponent } from "../vital-signs/vital-signs.component";
+import { GlobalSearchFieldComponent } from "../patient-header/global-search-field/global-search-field.component";
+import { NgIf } from "@angular/common";
+import { HelpService } from "../services/help.service";
+import { HelpComponent } from "../shared/help/help.component";
 
 @Component({
   selector: 'app-preventive-medical-checkup',
@@ -25,17 +26,17 @@ import {HelpComponent} from "../shared/help/help.component";
   styleUrl: './preventive-medical-checkup.component.css',
   encapsulation: ViewEncapsulation.None
 })
-export class PreventiveMedicalCheckupComponent implements OnInit{
+export class PreventiveMedicalCheckupComponent implements OnInit {
 
-  constructor(private helpService: HelpService) {
-  }
+  constructor(private helpService: HelpService) { }
+
   showOverlay = false;
   showPoint = false;
 
   @HostListener('window:keydown', ['$event'])
   handleKeyDown(event: KeyboardEvent) {
     if (event.key === 'F1') {
-      event.preventDefault(); // Prevent the default behavior of F1 key
+      event.preventDefault();
       this.showOverlay = !this.showOverlay;
       this.helpService.toggleOverlay(this.showOverlay);
     }
@@ -47,9 +48,9 @@ export class PreventiveMedicalCheckupComponent implements OnInit{
     });
   }
 
-
-  getToolTipText(tab: string) {
-    return tab === 'overview' ? 'In diesem Tab werden die wichtigsten Informationen des Patienten in tabellarischer Form angezeigt. Dazu gehören: Medikation: Eine Liste aller aktuellen Medikamente des Patienten, einschließlich Dosierung und Einnahmezeitraum. Diagnosen: Eine Aufstellung der aktuellen Diagnosen des Patienten. Laborparameter: Ergebnisse von Laboruntersuchungen des Patienten. Prozeduren: Durchgeführte Eingriffe und medizinische Prozeduren. Um eine Tabelle zu maximieren, klicken Sie auf das Maximieren-Symbol in der rechten oberen Ecke einer minimierten Tabelle. Die Tabellen können nach den Spaltenüberschriften durch einen Klick darauf sortiert werden.':
-      'In diesem Tab werden die verschiedenen Vitalparameter des Patienten angezeigt. Dazu gehören Messwerte wie Größe, Gewicht, Blutdruck, Herzfrequenz und andere wichtige Indikatoren, die den aktuellen Gesundheitszustand des Patienten widerspiegeln.'
+  getToolTipText(tab: string): string {
+    return tab === 'overview'
+      ? 'In diesem Tab werden die wichtigsten Informationen des Patienten in tabellarischer Form angezeigt. Dazu gehören: Medikation: Eine Liste aller aktuellen Medikamente des Patienten, einschließlich Dosierung und Einnahmezeitraum. Diagnosen: Eine Aufstellung der aktuellen Diagnosen des Patienten. Laborparameter: Ergebnisse von Laboruntersuchungen des Patienten. Prozeduren: Durchgeführte Eingriffe und medizinische Prozeduren. Um eine Tabelle zu maximieren, klicken Sie auf das Maximieren-Symbol in der rechten oberen Ecke einer minimierten Tabelle. Die Tabellen können nach den Spaltenüberschriften durch einen Klick darauf sortiert werden.'
+      : 'In diesem Tab werden die verschiedenen Vitalparameter des Patienten angezeigt. Dazu gehören Messwerte wie Größe, Gewicht, Blutdruck, Herzfrequenz und andere wichtige Indikatoren, die den aktuellen Gesundheitszustand des Patienten widerspiegeln.';
   }
 }
